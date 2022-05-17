5abeff5ae5274ec6a9a7cc65585bf61e25e98406
Tancrede Guillou <tancredeguillou@gmail.com>
2022-05-17 15:36:58 +0200

    process only in train

--- scripts/download_and_process_video.sh
+++ scripts/download_and_process_video.sh
@@ -15,8 +15,9 @@ mkdir $VIDEO_DIR
 #wget -O $DATASETS_DIR/face_reconstruction_video.mp4 "https://www.dropbox.com/s/htjtuhgpewgcmlh/videoplayback.mp4?dl=1"
 #mv $DATASETS_DIR/face_reconstruction_video.mp4 $VIDEO_DIR;
 
-mkdir $VIDEO_DIR/validation; mkdir $VIDEO_DIR/validation/original; mv $VIDEO_DIR/val/original/* $VIDEO_DIR/validation/original;
+mkdir $VIDEO_DIR/train;
+mkdir $VIDEO_DIR/train/input;
+mkdir $VIDEO_DIR/train/output;
 
+# Process the whole video
 bsub -n 1 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=10240]" -o train.txt python face_data_processor.py --videos_dir $VIDEO_DIR --use_canny_edges $USE_CANNY_EDGES --split train
-bsub -n 1 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=10240]" -o val.txt python face_data_processor.py --videos_dir $VIDEO_DIR --use_canny_edges $USE_CANNY_EDGES --split validation
-bsub -n 1 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=10240]" -o test.txt python face_data_processor.py --videos_dir $VIDEO_DIR --use_canny_edges $USE_CANNY_EDGES --split test
